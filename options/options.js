89f0bce1e42f797e2b7a123254c3412210746cee
Crowdbotics <team@crowdbotics.com>
2024-01-18 08:29:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "micolearnpro-45426",
+  url: "https://micolearnpro-45426.botics.co",
+  api: "https://micolearnpro-45426.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
